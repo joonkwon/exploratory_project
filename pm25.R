abbbcf7f32894c15eb48deadb92a0d130b2241f6
Joon Kwon <joon.kwon@bellmedia.ca>
2016-02-24 16:58:25 -0500

    pm25.R basic Idea completed

--- pm25.R
+++ pm25.R
@@ -22,13 +22,21 @@ library(ggplot2)
 g <- ggplot(pm25_bal_sum, aes(year, sum_emission))
 g + geom_bar(stat="identity") + facet_grid(.~type)
 
-scc$all_text <- paste(scc$Short.Name, scc$SCC.Level.One, scc$SCC.Level.Two, 
-            scc$SCC.Level.Three, scc$SCC.Level.Four, 
-            scc$EI.Sector, sep=" ")
-
-scc.comb.coal.lg<- grepl("combustion", scc$all_text, ignore.case = TRUE) &
-                grepl("coal", scc$all_text, ignore.case = TRUE)
 pm25.comb.coal <- subset(pm25, pm25$SCC %in% scc[scc.comb.coal.lg,]$SCC)
 pm25.comb.coal.yearly <- tapply(pm25$Emissions,pm25$year, sum, na.rm=TRUE)
 barplot(pm25.comb.coal.yearly)
 
+scc.vehicle.lg <- (grepl("vehicle", scc$all_text, ignore.case = TRUE) 
+        | grepl("vessel", scc$all_text, ignore.case = TRUE))
+
+pm25_bal_veh <- subset(pm25_baltimore, pm25_baltimore$SCC %in% scc[scc.vehicle.lg,]$SCC )
+pm25_bal_veh_tot <- tapply(pm25_bal_veh$Emissions, pm25_bal_veh$year, sum, na.rm=TRUE)
+color <- rgb(0,0,1, alpha=0.3)
+barplot(pm25_bal_veh_tot, col=color, xlab="Year", ylab="Vehicle PM25 Emission (ton)", 
+        border="red", main="Vehicle PM25 Emission in Baltimore")
+
+pm25_ca_veh <- subset(pm25, pm25$fips == "06037" & pm25$SCC %in%  scc[scc.vehicle.lg,]$SCC)
+pm25_ca_veh_tot <- tapply(pm25_ca_veh$Emissions, pm25_ca_veh$year, sum, na.rm=TRUE)
+color <- rgb(0,0,1, alpha=0.3)
+barplot(pm25_ca_veh_tot, col=color, xlab="Year", ylab="Vehicle PM25 Emission (ton)", 
+        border="red", main="Vehicle PM25 Emission in California")
